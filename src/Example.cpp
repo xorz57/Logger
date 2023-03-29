1c18f124e632cc9a8f4f7178f1843dcb515e2dde
George Fotopoulos <84932056+xorz57@users.noreply.github.com>
2023-03-30 01:01:30 +0300

    Update Example.cpp

--- src/Example.cpp
+++ src/Example.cpp
@@ -2,13 +2,16 @@
 
 int main() {
     Logger::SetLevel(Logger::Level::Trace);
+
     Logger::EnableFileOutput();
     Logger::SetFileOutput("Example.log");
+
     Logger::Trace("trace");
     Logger::Debug("debug");
     Logger::Info("info");
     Logger::Warn("warn");
     Logger::Error("error");
     Logger::Critical("critical");
+
     return 0;
 }
